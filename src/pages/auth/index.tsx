a5f2b572418000daaeb6b365e0075fc3f1e0c79e
fernandesMM <roberto.fernandes@madeiramadeira.com.br>
2022-06-29 21:27:06 -0300

    [Feat] Insert Cache

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 
 import { requests } from "../../utils/requests";
+import { cache } from "../../utils/cache";
 
 import { Container, Spinner } from "../../components/general"
 import { Box, Title, Text, Input, Button, Form } from "./styles"
@@ -11,9 +12,6 @@ import { setUserName } from "../../store/reducers/user";
 export const AuthPage = () => {
     const dispatch = useDispatch();
     const user = useSelector((state: RootState) => state.user);
-
-    console.log(`User: ${user}`);
-
     const [ pokemon, setPokemon ] = useState<any>(null);
 
     const fetchPokemonHome = async () => {
@@ -24,6 +22,12 @@ export const AuthPage = () => {
             random = await requests.get.pokemon();
             setPokemon(random);
         }, 5000);
+    }
+
+    const handleAuth = (event: any) => {
+        event.preventDefault();
+        if (!user.name) return alert('Insira um nome');
+        cache.set('user', user);
     }
 
     useEffect(() => {
@@ -43,7 +47,7 @@ export const AuthPage = () => {
                             <Spinner></Spinner>
                         </Box>
                 }
-                <Form onSubmit={(e) => e.preventDefault()}>
+                <Form onSubmit={(event: any) => handleAuth(event)}>
                     <Text>Bem vindo ao</Text>
                     <Title>Poke<strong>Goal</strong></Title>
                     <Input placeholder="Insira seu nome" onChange={(e) => dispatch(setUserName(e.target.value))} />
